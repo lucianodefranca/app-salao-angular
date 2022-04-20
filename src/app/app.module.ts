541addcdd3b9d41e5e87b84ba34654717365631d
luciano de franca <lucianodefranca86@gmail.com>
2022-04-19 23:59:28 -0300

    Component sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,22 +6,31 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+
 
 
 import { HeaderComponent } from './views/template/header/header.component';
 import { FooterComponent } from './views/template/footer/footer.component';
+import { SidenavComponent } from './views/template/sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
